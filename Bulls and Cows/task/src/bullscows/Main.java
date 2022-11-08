76958edf1d19064903da0965fcabe16bf387bc04
Mateusz <zubermateusz@gmail.com>
2022-11-08 18:15:38 +0100

    stage 1/7

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -2,6 +2,23 @@ package bullscows;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("The secret code is prepared: ****.");
+
+        System.out.println("Turn 1. Answer:\n" +
+                "1234\n" +
+                "Grade: 1 cow.");
+
+        System.out.println("Turn 3. Answer:\n" +
+                "9012\n" +
+                "Grade: 1 bull and 1 cow.");
+
+        System.out.println("Turn 7. Answer:\n" +
+                "9305\n" +
+                "Grade: 4 bulls.");
+        System.out.println("Turn 7. Answer:\n" +
+                "9305\n" +
+                "Grade: 4 bulls.");
+        System.out.println("Congrats! The secret code is 9305.");
     }
 }
