7f4c033ae905c1b398a9f5a8a1f6de040622b6d5
Mateusz <zubermateusz@gmail.com>
2022-11-08 22:14:01 +0100

    dodanie generowania kodu z Random finished 3/7

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,7 +1,9 @@
 package bullscows;
 
-import java.util.Arrays;
-import java.util.Scanner;
+
+
+import java.util.*;
+
 
 public class Main {
 
@@ -10,15 +12,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] secretCode = {2, 4, 7, 3}; // = new int[4]; // inicjalizacja miejsca na secretCode
+        Scanner scanner = new Scanner(System.in);
+        int numberOfCodeDigits = scanner.nextInt(); // pobranie od gracza dlugosci kodu
+
+        int[] secretCode = generateCode(numberOfCodeDigits);//{2, 4, 7, 3}; // = new int[4]; // inicjalizacja miejsca na secretCode
 
         //secretCode = getCodeFromPlayer(); // pobranie SecretCode od gracza i wpisanie do tablicy
 
-        int[] guessCode = getCodeFromPlayer(); // pobranie quessCode od gracza i wpisanie do tablicy
-        checkCodes(secretCode, guessCode); // porownanie kodow
+        //int[] guessCode = getCodeFromPlayer(); // pobranie quessCode od gracza i wpisanie do tablicy
+        //checkCodes(secretCode, guessCode); // porownanie kodow
 
-        endGame(secretCode, bull, cows); // wyswietlenie wyniku konczacego rozgrywke
-
+        //endGame(secretCode, bull, cows); // wyswietlenie wyniku konczacego rozgrywke
+        win(secretCode);
 
 /*        System.out.println("The secret code is prepared: ****.");
 
@@ -41,6 +46,25 @@ public class Main {
 */
     }
 
+    private static int[] generateCode(int codeLength) {
+        int[] code = new int[codeLength];
+        if (codeLength > 10) {
+            System.out.println("Error: can't generate a secret number with a " +
+                    "length of " + codeLength + " because there aren't enough unique digits.");
+            System.exit(0);
+        } else {
+            int[] tempCode = new Random().ints(0, 10)
+                    .distinct()
+                    .limit(codeLength + 1).toArray();
+            if (tempCode[0] != 0) {
+                System.arraycopy(tempCode, 0, code, 0, code.length);
+            } else {
+                System.arraycopy(tempCode, 1, code, 0, code.length);
+            }
+        }
+        return code;
+    }
+
     private static void checkCodes(int[] secretCode, int[] guessCode) {
         bull = 0;
         cows = 0;
@@ -58,33 +82,33 @@ public class Main {
                 }
             }
         }
-    }
+    }//porowanie kodow i przypisanie buuls i cows
 
-    private static int[] getCodeFromPlayer() { // pobranie kodu do porównania od gracza
+    private static int[] getCodeFromPlayer() {
         Scanner scanner = new Scanner(System.in);
         int[] code = Arrays.stream(scanner.nextLine().split("")).mapToInt(Integer::parseInt).toArray();
         return code;
-    }
+    }// pobranie kodu do porównania od gracza
 
-    private static void win(int[] secretCode) {
-        System.out.printf("The secret code is %d.", secretCode);
-    }
+    private static void win(int[] code) {
+        System.out.printf("The random secret code is %s.", printCode(code));
+    } // wyswietlenie The secret code is secretCode
 
     private static void endGame(int[] code, int bull, int cows) {
-        System.out.println("Grade: ");
+        System.out.print("Grade: ");
         if (bull == 0 && cows == 0) {
             System.out.print("None. ");
         }
         if (bull > 0) System.out.print(bull + " bull(s)");
         if (cows > 0) System.out.print(" and " + cows + " cow(s). ");
         System.out.print("The secret code is ".concat(printCode(code)));
-    }
+    } //wyswietlenie Grade:/None.  bull(s) and cow(s). The secret code is code
 
-    private static String printCode(int[] code) { //wyswietlenie tablicy 1234
+    private static String printCode(int[] code) {
         String text = "";
-        for (int number : code) {
-            text += number;
+        for (int digit : code) {
+            text += digit;
         }
         return text;
-    }
+    }//wyswietlenie tablicy 1234
 }
