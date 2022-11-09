817c61d7820ec11b5bca606cffda425379374da1
Mateusz <zubermateusz@gmail.com>
2022-11-09 14:31:36 +0100

    add converADate

---
+++ Topics/Processing strings/Convert a date/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String data = scanner.nextLine();
+        System.out.print(data.charAt(5));
+        System.out.print(data.charAt(6));
+        System.out.print("/");
+        System.out.print(data.charAt(8));
+        System.out.print(data.charAt(9));
+        System.out.print("/");
+        System.out.print(data.charAt(0));
+        System.out.print(data.charAt(1));
+        System.out.print(data.charAt(2));
+        System.out.print(data.charAt(3));
+
+    }
+}
