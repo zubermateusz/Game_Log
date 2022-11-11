43affc23e43d64271ff3f5f4d5c2b02f447e51cd
Mateusz <zubermateusz@gmail.com>
2022-11-11 18:46:36 +0100

    corrected generatePassword

---
+++ Topics/Processing strings/Generating passwords/src/Main.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.util.random.RandomGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        /*
+        * A uppercase letters, at least B lowercase letters, at least C digits and consists of exactly N symbols
+        * */
+
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[4];
+        arr[0] = scanner.nextInt();
+        arr[1] = scanner.nextInt();
+        arr[2] = scanner.nextInt();
+        arr[3] = scanner.nextInt();
+
+
+        String code = "";
+
+        //tablice znakow
+        String a = "AB";
+        String b = "ab";
+        String c = "12";
+
+        String tempCode = "";
+        for (int i = 0; tempCode.length() < arr[0]; i++) { //wypelnienie ABABABAB
+            tempCode += a;
+        }
+        code += tempCode.substring(0, arr[0]);
+
+        tempCode = "";
+        for (int i = 0; tempCode.length() < arr[1]; i++) { // wypelnienei ababababab
+            tempCode += b;
+        }
+        code += tempCode.substring(0, arr[1]);
+
+        tempCode = "";
+        for (int i = 0; tempCode.length() < arr[2]; i++) { // wypelnienie 121212121212
+            tempCode += c;
+        }
+        code += tempCode.substring(0, arr[2]);
+
+
+
+        for (int i = 0; code.length() < arr[3]; i++) { //dokonczenie ABABABA
+            if (code.length() == 0) {
+                code += a;
+            } else {
+                if (code.charAt(code.length() - 1) == a.charAt(0)) {
+                    code += a.charAt(1);
+                }
+                code += a;
+            }
+        }
+        code = code.substring(0, arr[3]);
+
+
+
+        System.out.println(code);
+    }
+}
