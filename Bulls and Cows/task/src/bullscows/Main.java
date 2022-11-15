96724576fc39f058928ce9d6b8eb92204a5de5b3
Mateusz <zubermateusz@gmail.com>
2022-11-15 13:59:59 +0100

    nie dziala przy podaniu kodu dlugosci == 1

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -13,9 +13,20 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter the secret code's length:");
         int numberOfCodeDigits = scanner.nextInt(); // pobranie od gracza dlugosci kodu
 
         int[] secretCode = generateCode(numberOfCodeDigits);//{2, 4, 7, 3}; // = new int[4]; // inicjalizacja miejsca na secretCode
+        //System.out.println(printCode(secretCode));
+        int[] playerCode = new int[numberOfCodeDigits];
+        System.out.println("Okay, let's start a game!");
+        int turn = 0;
+        do {
+            System.out.println("Turn " + ++turn + ":");
+            playerCode = getCodeFromPlayer();
+            checkCodes(secretCode, playerCode);
+            endGame(secretCode, bull, cows);
+        } while (bull != numberOfCodeDigits);
 
         //secretCode = getCodeFromPlayer(); // pobranie SecretCode od gracza i wpisanie do tablicy
 
@@ -23,7 +34,7 @@ public class Main {
         //checkCodes(secretCode, guessCode); // porownanie kodow
 
         //endGame(secretCode, bull, cows); // wyswietlenie wyniku konczacego rozgrywke
-        win(secretCode);
+        //win(secretCode);
 
 /*        System.out.println("The secret code is prepared: ****.");
 
@@ -53,14 +64,13 @@ public class Main {
                     "length of " + codeLength + " because there aren't enough unique digits.");
             System.exit(0);
         } else {
-            int[] tempCode = new Random().ints(0, 10)
-                    .distinct()
-                    .limit(codeLength + 1).toArray();
-            if (tempCode[0] != 0) {
-                System.arraycopy(tempCode, 0, code, 0, code.length);
-            } else {
-                System.arraycopy(tempCode, 1, code, 0, code.length);
+            StringBuilder digitBase = new StringBuilder("1234567890");
+            for (int i = 0; i < codeLength; i++) {
+                int temp = new Random().nextInt(digitBase.length());
+                code[i] = Integer.parseInt(digitBase.charAt(temp) + "");
+                digitBase.deleteCharAt(temp);
             }
+
         }
         return code;
     }
@@ -82,7 +92,7 @@ public class Main {
                 }
             }
         }
-    }//porowanie kodow i przypisanie buuls i cows
+    }//porowanie kodow i przypisanie bulls i cows
 
     private static int[] getCodeFromPlayer() {
         Scanner scanner = new Scanner(System.in);
@@ -99,9 +109,17 @@ public class Main {
         if (bull == 0 && cows == 0) {
             System.out.print("None. ");
         }
-        if (bull > 0) System.out.print(bull + " bull(s)");
-        if (cows > 0) System.out.print(" and " + cows + " cow(s). ");
-        System.out.print("The secret code is ".concat(printCode(code)));
+        if (bull == code.length) {
+            System.out.println("Congratulations! You guessed the secret code.");
+        } else {
+            if (bull == 1) System.out.print(bull + " bull");
+            if (cows == 1) System.out.print(" and " + cows + " cow");
+
+            if (bull > 1) System.out.print(bull + " bull(s)");
+            if (cows > 1) System.out.print(" and " + cows + " cow(s). ");
+        }
+        // System.out.print("The secret code is ".concat(printCode(code)));
+        System.out.println();
     } //wyswietlenie Grade:/None.  bull(s) and cow(s). The secret code is code
 
     private static String printCode(int[] code) {
