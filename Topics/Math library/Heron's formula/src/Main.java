382dc39d8ae3ae20925e7abba63fd8142cc9d990
Mateusz <zubermateusz@gmail.com>
2022-11-16 19:12:57 +0100

    add Heron's formula

---
+++ Topics/Math library/Heron's formula/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        double p = (a + b + c) / 2;
+        double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        System.out.println(result);
+    }
+}
