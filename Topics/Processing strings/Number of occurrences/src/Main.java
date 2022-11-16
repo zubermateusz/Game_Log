4ad439b7289b9b169e87618deba1241ea7418e50
Mateusz <zubermateusz@gmail.com>
2022-11-16 12:31:02 +0100

    add Number of occurrences

---
+++ Topics/Processing strings/Number of occurrences/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder text = new StringBuilder(scanner.nextLine());
+        String textInside = scanner.nextLine();
+        int count = 0;
+        int textIndex = text.indexOf(textInside);
+        while (textIndex != -1) {
+            count++;
+            text.delete(textIndex, textIndex + textInside.length());
+            textIndex = text.indexOf(textInside);
+        }
+        System.out.println(count);
+    }
+}
