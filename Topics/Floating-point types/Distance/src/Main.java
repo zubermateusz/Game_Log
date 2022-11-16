12e455f6cd2df4ccaa178e7394f87eec4ef4d1d9
Mateusz <zubermateusz@gmail.com>
2022-11-16 18:12:26 +0100

    add Distance

---
+++ Topics/Floating-point types/Distance/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        System.out.println(a / b);
+    }
+}
