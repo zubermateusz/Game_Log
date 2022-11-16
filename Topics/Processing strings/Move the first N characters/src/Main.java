554c6f860a374e59ef579f761ba6d6597187738e
Mateusz <zubermateusz@gmail.com>
2022-11-16 14:07:57 +0100

    add Move the first N characters

---
+++ Topics/Processing strings/Move the first N characters/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+        int digit = scanner.nextInt();
+        if (digit > text.length()) digit = text.length();
+        System.out.println(text.substring(digit) + text.substring(0, digit));
+    }
+}
