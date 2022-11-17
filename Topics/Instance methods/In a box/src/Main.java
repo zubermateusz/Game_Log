b4f0ff43ce1f096a7c88e4102cf04f3448b143d4
Mateusz <zubermateusz@gmail.com>
2022-11-17 12:57:25 +0100

    add In a box

---
+++ Topics/Instance methods/In a box/src/Main.java
@@ -0,0 +1,12 @@
+
+class Box {
+
+    double height;
+    double width;
+    double length;
+
+    // write a method here
+    double getVolume() {
+        return  this.height * this.width * this.length;
+    }
+}
