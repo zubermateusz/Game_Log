20e46fe46cbd76f3e8918889c766096d0c1359fa
Mateusz <zubermateusz@gmail.com>
2022-11-17 17:24:16 +0100

    add Binary search

---
+++ Topics/Package/Binary search/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class BinarySearch {
+    /**
+     * @param nums ordered sequence of integers
+     * @param key  an element for searching
+     * @return index of key or a negative value
+     */
+    public static int callBinarySearch(int[] nums, int key) {
+        // write your code here
+        return Arrays.binarySearch(nums, key);
+    }
+}
