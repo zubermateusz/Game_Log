349064ffe0c99f596c944f3d25f7a7381b734185
Mateusz <zubermateusz@gmail.com>
2022-11-17 09:21:37 +0100

    add Account

---
+++ Topics/Constructor/Account/src/Main.java
@@ -0,0 +1,13 @@
+
+class Account {
+
+    long balance;
+    String ownerName;
+    boolean locked;
+
+    Account(long balance, String ownerName, boolean locked) {
+        this.balance = balance;
+        this.ownerName = ownerName;
+        this.locked = locked;
+    }
+} // write your class here
